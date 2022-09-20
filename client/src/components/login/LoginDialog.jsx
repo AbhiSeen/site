99a744e1d9d090923bd959f5ff92e515d046aa05
Suraj Prabhu <prabhusuraj103@gmail.com>
2022-09-20 10:02:05 +0530

    Update LoginDialog.jsx

--- client/src/components/login/LoginDialog.jsx
+++ client/src/components/login/LoginDialog.jsx
@@ -1,189 +1,261 @@
-import React, { useState,useContext} from 'react';
-import { Dialog, DialogContent, TextField, Box, Button, Typography, styled } from '@mui/material';
-import {  authenticateLogin,authenticateSignup } from '../service/api.js';
-import {DataContext} from '../../context/DataProvider'
+import React, { useState, useContext } from "react";
+import {
+  Dialog,
+  DialogContent,
+  TextField,
+  Box,
+  Button,
+  Typography,
+  styled,
+} from "@mui/material";
+import { authenticateLogin, authenticateSignup } from "../service/api.js";
+import { DataContext } from "../../context/DataProvider";
 
 import { useNavigate } from "react-router-dom";
 
-
 const Component = styled(DialogContent)`
-    height: 70vh;
-    width: 90vh;
-    padding: 0;
-    padding-top: 0;
+  height: 70vh;
+  width: 90vh;
+  padding: 0;
+  padding-top: 0;
 `;
 const Image = styled(Box)`
-    background: #2874f0 url(https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/login_img_c4a81e.png) center 85% no-repeat;
-    width: 25%;
-    height: 80%;
-    padding: 45px 35px;
-    & > p, & > h5 {
-        color: #FFFFFF;
-        font-weight: 600
-    }
+  background: #2874f0
+    url(https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/login_img_c4a81e.png)
+    center 85% no-repeat;
+  width: 25%;
+  height: 80%;
+  padding: 45px 35px;
+  & > p,
+  & > h5 {
+    color: #ffffff;
+    font-weight: 600;
+  }
 `;
 const Wrapper = styled(Box)`
-    padding: 25px 35px;
-    display: flex;
-    flex: 1;
-    overflow: auto;
-    flex-direction: column;
-    & > div, & > button, & > p {
-        margin-top: 20px;
-    }
+  padding: 25px 35px;
+  display: flex;
+  flex: 1;
+  overflow: auto;
+  flex-direction: column;
+  & > div,
+  & > button,
+  & > p {
+    margin-top: 20px;
+  }
 `;
 const LoginButton = styled(Button)`
-    text-transform: none;
-    background: #FB641B;
-    color: #fff;
-    height: 48px;
-    border-radius: 2px;
+  text-transform: none;
+  background: #fb641b;
+  color: #fff;
+  height: 48px;
+  border-radius: 2px;
 `;
 
 const RequestOTP = styled(Button)`
-    text-transform: none;
-    background: #fff;
-    color: #2874f0;
-    height: 48px;
-    border-radius: 2px;
-    box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
+  text-transform: none;
+  background: #fff;
+  color: #2874f0;
+  height: 48px;
+  border-radius: 2px;
+  box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
 `;
 const Text = styled(Typography)`
-    color: #878787;
-    font-size: 12px;
+  color: #878787;
+  font-size: 12px;
 `;
 const CreateAccount = styled(Typography)`
-    margin: auto 0 5px 0;
-    text-align: center;
-    color: #2874f0;
-    font-weight: 600;
-    font-size: 14px;
-    cursor: pointer
-`
+  margin: auto 0 5px 0;
+  text-align: center;
+  color: #2874f0;
+  font-weight: 600;
+  font-size: 14px;
+  cursor: pointer;
+`;
 
 const Error = styled(Typography)`
-    font-size: 20px;
-    color: #ff6161;
-    line-height: 0;
-    margin-top: 10px;
-    font-weight: 600;
-`
+  font-size: 20px;
+  color: #ff6161;
+  line-height: 0;
+  margin-top: 10px;
+  font-weight: 600;
+`;
 
 const loginInitialValues = {
-    username: '',
-    password: ''
+  username: "",
+  password: "",
 };
 
 const signupInitialValues = {
-    firstname: '',
-    lastname: '',
-    username: '',
-    email: '',
-    password: '',
-    phone: ''
+  firstname: "",
+  lastname: "",
+  username: "",
+  email: "",
+  password: "",
+  phone: "",
 };
 const accountInitialValues = {
-    login: {
-        view: 'login',
-        heading: 'Login',
-        subHeading: 'Get access to your Orders, Wishlist and Recommendations'
-    },
-    signup: {
-        view: 'signup',
-        heading: "Looks like you're new here",
-        subHeading: 'Signup to get started'
-    }
-}
+  login: {
+    view: "login",
+    heading: "Login",
+    subHeading: "Get access to your Orders, Wishlist and Recommendations",
+  },
+  signup: {
+    view: "signup",
+    heading: "Looks like you're new here",
+    subHeading: "Signup to get started",
+  },
+};
 
 const LoginDialog = ({ open, setOpen }) => {
-    const [ account, toggleAccount ] = useState(accountInitialValues.login);
-    const [disabled,setDisabled]=useState({});
-    const [ login, setLogin ] = useState(loginInitialValues);
-    const [ signup, setSignup ] = useState(signupInitialValues);
-    const [ error, setError] = useState(false);
-    const {setAccount}   = useContext(DataContext);
-    const navigate=useNavigate()
+  const [account, toggleAccount] = useState(accountInitialValues.login);
+  const [disabled, setDisabled] = useState({});
+  const [login, setLogin] = useState(loginInitialValues);
+  const [signup, setSignup] = useState(signupInitialValues);
+  const [error, setError] = useState(false);
+  const { setAccount } = useContext(DataContext);
+  const navigate = useNavigate();
 
-    const handleClose = () => {
-        setOpen(false);
-        toggleAccount(accountInitialValues.login);
-        setError(false);
-       
-    }
-    const toggleSignup = () => {
-        toggleAccount(accountInitialValues.signup);
-    }
+  const handleClose = () => {
+    setOpen(false);
+    toggleAccount(accountInitialValues.login);
+    setDisabled({ ...disabled, login: false });
+    setError(false);
+  };
+  const toggleSignup = () => {
+    toggleAccount(accountInitialValues.signup);
+  };
 
-    const onInputChange = (e) => {
-        setSignup({ ...signup, [e.target.name]: e.target.value });
-    }
-    const onValueChange = (e) => {
-        setLogin({ ...login, [e.target.name]: e.target.value });
-    }
+  const onInputChange = (e) => {
+    setSignup({ ...signup, [e.target.name]: e.target.value });
+  };
+  const onValueChange = (e) => {
+    setLogin({ ...login, [e.target.name]: e.target.value });
+  };
 
-    const loginUser = async() => {
-        setDisabled({...disabled,login:true});
-        const response = await authenticateLogin(login);  
-        const  {authToken,firstname,referrals}=response.data;   
-        if(response.status === 200 && login.username!=="admin") {
-            localStorage.setItem("token",authToken)
-            setTimeout(()=>{
-                setAccount(firstname);
-                handleClose();
-            },1000)
-            
-        }else if(login.username==='admin'){
-            localStorage.setItem("adminToken",authToken)
-            setOpen(false);
-            navigate("/dashboard")
-        }
-        else {
-            setDisabled({...disabled,login:false});
-            setError(true);
-        }
+  const loginUser = async () => {
+    setDisabled({ ...disabled, login: true });
+    const response = await authenticateLogin(login);
+    const { authToken, firstname} = response.data;
+    if (response.status === 200 && login.username !== "admin" && authToken) {
+      localStorage.setItem("token", authToken);
+      setTimeout(() => {
+        setAccount(firstname);
+        handleClose();
+      }, 1000);
+    } else if (login.username === "admin" && authToken) {
+      localStorage.setItem("adminToken", authToken);
+      setOpen(false);
+      navigate("/dashboard");
+    } else {
+      setDisabled({ ...disabled, login: false });
+      setError(true);
     }
+  };
 
-    const signupUser = async() => {
-        setDisabled({...disabled,signUp:true});
-        let response = await authenticateSignup(signup);
-        if(!response) { setDisabled({...disabled,signUp:false});}
+  const signupUser = async () => {
+    setDisabled({ ...disabled, signUp: true });
+    let response = await authenticateSignup(signup);
+    if (!response) {
+      return;
     }
-    return (
-        <Dialog open={open} onClose={handleClose} PaperProps={{ sx: { maxWidth: 'unset' } }}>
-            <Component>
-                <Box style={{ display: 'flex', height: '100%' }}>
-                    <Image>
-                        <Typography variant="h5">{account.heading}</Typography>
-                        <Typography style={{ marginTop: 20 }}>{account.subHeading}</Typography>
-                    </Image>
-                    {  
-                      account.view === 'login' ? 
-                        <Wrapper>
-                            <TextField variant="standard" onChange={(e) => onValueChange(e)} name='username' label="Enter UserName" />
-                            { error && <Error>Please enter valid UserName</Error> }
-                            <TextField variant="standard" onChange={(e) => onValueChange(e)} name='password' label="Enter Password" />
-                            <Text>By continuing, you agree to  AMFashion's Terms of Use and Privacy Policy.</Text>
-                            <LoginButton onClick={() => loginUser() } disabled={disabled.login || false } >Login</LoginButton>
-                            <Typography style={{ textAlign: 'center' }}>OR</Typography>
-                            <RequestOTP >Request OTP</RequestOTP>
-                            <CreateAccount onClick={() => toggleSignup()}>New to AMFashion's? Create an account</CreateAccount>
-                        </Wrapper>
-                        :
-                        <Wrapper>
-                            <TextField variant="standard" onChange={(e) => onInputChange(e)}  name='firstname' label="Enter FirstName" />
-                            <TextField variant="standard" onChange={(e) => onInputChange(e)}  name='lastname' label="Enter LastName" />
-                            <TextField variant="standard" onChange={(e) => onInputChange(e)}  name='username' label="Enter UserName" />
-                            <TextField variant="standard" onChange={(e) => onInputChange(e)}  name='email' label="Enter  Email" />
-                            <TextField variant="standard" onChange={(e) => onInputChange(e)}  name='password'label="Enter Password" />
-                            <TextField variant="standard" onChange={(e) => onInputChange(e)}  name='phone' label="Enter Mobile Number" />
-                            <LoginButton onClick={() => signupUser()}  disabled={disabled.signUp || false} >Let's Shop</LoginButton>
-                        </Wrapper>
-                    }
-                </Box>
-            </Component>
+    setDisabled({ ...disabled, signUp: false });
+  };
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      PaperProps={{ sx: { maxWidth: "unset" } }}
+    >
+      <Component>
+        <Box style={{ display: "flex", height: "100%" }}>
+          <Image>
+            <Typography variant="h5">{account.heading}</Typography>
+            <Typography style={{ marginTop: 20 }}>
+              {account.subHeading}
+            </Typography>
+          </Image>
+          {account.view === "login" ? (
+            <Wrapper>
+              <TextField
+                variant="standard"
+                onChange={(e) => onValueChange(e)}
+                name="username"
+                label="Enter UserName"
+              />
+              {error && <Error>Please enter valid UserName</Error>}
+              <TextField
+                variant="standard"
+                onChange={(e) => onValueChange(e)}
+                name="password"
+                label="Enter Password"
+              />
+              <Text>
+                By continuing, you agree to AMFashion's Terms of Use and Privacy
+                Policy.
+              </Text>
+              <LoginButton
+                onClick={() => loginUser()}
+                disabled={disabled.login || false}
+              >
+                Login
+              </LoginButton>
+              <Typography style={{ textAlign: "center" }}>OR</Typography>
+              <RequestOTP>Request OTP</RequestOTP>
+              <CreateAccount onClick={() => toggleSignup()}>
+                New to AMFashion's? Create an account
+              </CreateAccount>
+            </Wrapper>
+          ) : (
+            <Wrapper>
+              <TextField
+                variant="standard"
+                onChange={(e) => onInputChange(e)}
+                name="firstname"
+                label="Enter FirstName"
+              />
+              <TextField
+                variant="standard"
+                onChange={(e) => onInputChange(e)}
+                name="lastname"
+                label="Enter LastName"
+              />
+              <TextField
+                variant="standard"
+                onChange={(e) => onInputChange(e)}
+                name="username"
+                label="Enter UserName"
+              />
+              <TextField
+                variant="standard"
+                onChange={(e) => onInputChange(e)}
+                name="email"
+                label="Enter  Email"
+              />
+              <TextField
+                variant="standard"
+                onChange={(e) => onInputChange(e)}
+                name="password"
+                label="Enter Password"
+              />
+              <TextField
+                variant="standard"
+                onChange={(e) => onInputChange(e)}
+                name="phone"
+                label="Enter Mobile Number"
+              />
+              <LoginButton
+                onClick={() => signupUser()}
+                disabled={disabled.signUp || false}
+              >
+                Let's Shop
+              </LoginButton>
+            </Wrapper>
+          )}
+        </Box>
+      </Component>
+    </Dialog>
+  );
+};
 
-        </Dialog>
-    )
-}
-
-export default LoginDialog
+export default LoginDialog;
